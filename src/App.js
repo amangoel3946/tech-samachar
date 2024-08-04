0861b2a2e214507ec14daf99ccb6afcfebdab7bb
amangoel3946 <amangoel3946@gmail.com>
2024-08-04 17:38:57 +0530

    Aligned the components in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+// import Search from "./Search";
+import Search from "./search.js";
+import Pagination from "./Pagination";
+import Stories from "./Stories";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>App
+    <Search />
+    <Pagination />
+    <Stories />
+  </div>;
 };
 
 export default App;
